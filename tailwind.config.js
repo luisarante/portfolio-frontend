1e76b1c93cf28a47222cdd54acd328d2b84b4851
Luis Arantes <arantesluisfernando@gmail.com>
2025-11-05 08:53:38 -0300

    Estrutura inicial

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Ajuste isso para suas pastas
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
